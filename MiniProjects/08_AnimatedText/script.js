409b792d699379003cf637b2af8a2417fac44f5d
Slawomir <smajkut94@gmail.com>
2022-10-10 00:25:48 +0200

    Working at JS

---
+++ MiniProjects/08_AnimatedText/script.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.modal')
+const modal = document.querySelector('.modal-input')
+const modal = document.querySelector('.modal-btn')
+const modal = document.querySelector('.save-btn')
+const modal = document.querySelector('.text')
+const modal = document.querySelector('.error-msg')
+
+let inputValue = 'To jest testowa wiadomość'
+let timeout
+let index = 1
+let speed = 80
+
+const writingAnimation = () => {
+	text.innerHTML = inputValue.slice(0, index)
+
+	index++
+
+	if(index > 	)
+}
