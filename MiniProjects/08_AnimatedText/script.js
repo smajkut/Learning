5e87145b76deb8582348f883ef06f6b60dedff89
Slawomir <smajkut94@gmail.com>
2022-10-11 23:10:56 +0200

    Finish js

--- MiniProjects/08_AnimatedText/script.js
+++ MiniProjects/08_AnimatedText/script.js
@@ -1,19 +1,50 @@
 const modal = document.querySelector('.modal')
-const modal = document.querySelector('.modal-input')
-const modal = document.querySelector('.modal-btn')
-const modal = document.querySelector('.save-btn')
-const modal = document.querySelector('.text')
-const modal = document.querySelector('.error-msg')
+const input = document.querySelector('.modal-input')
+const modalBtn = document.querySelector('.modal-btn')
+const saveBtn = document.querySelector('.save-btn')
+const text = document.querySelector('.text')
+const errorMsg = document.querySelector('.error-msg')
 
 let inputValue = 'To jest testowa wiadomość'
 let timeout
 let index = 1
-let speed = 80
+let speed = 127
 
 const writingAnimation = () => {
 	text.innerHTML = inputValue.slice(0, index)
 
 	index++
 
-	if(index > 	)
+	if(index > 	inputValue.length) return
+
+	timeout = setTimeout(writingAnimation, speed)
 }
+
+const showModal = () => {
+	modal.classList.toggle('active')
+}
+
+const closeModal = () => {
+	
+	if(input.value == '') {
+		errorMsg.textContent = 'Wprowadź tekst'
+		return
+	}
+	inputValue = input.value
+	modal.classList.remove('active')
+	clearStuff()
+	writingAnimation()
+}
+
+const clearStuff = () => {
+	index = 1
+	clearTimeout(timeout)
+	input.value=''
+	errorMsg.textContent=''
+
+}
+
+
+modalBtn.addEventListener('click', showModal)
+saveBtn.addEventListener('click', closeModal)
+// writingAnimation()
