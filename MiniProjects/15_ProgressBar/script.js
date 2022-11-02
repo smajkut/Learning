689b9b48a8f4541c9288ef598baf20ef33791b71
Slawomir <smajkut94@gmail.com>
2022-11-02 18:51:20 +0100

    Finished project with ProgressBar

--- MiniProjects/15_ProgressBar/script.js
+++ MiniProjects/15_ProgressBar/script.js
@@ -1,3 +1,4 @@
+const btn = document.querySelector('.scroll-to-top')
 let root = document.querySelector(':root')
 let scroll
 let bodyD = document.body
@@ -13,18 +14,29 @@ let viewPortHeight
 let pxToScroll
 let pxProgress
 
-
-
 const scrollRefresh = () => {
-    scroll = window.scrollY
+	scroll = window.scrollY
 	viewPortHeight = window.innerHeight
 	pxToScroll = heightOfPage - viewPortHeight
-    pxProgress = (scroll / pxToScroll * 100) + '%'
-    
+	pxProgress = (scroll / pxToScroll) * 100 + '%'
+
 	// console.log(scroll)
 	// console.log(pxToScroll)
-    // console.log(pxProgress)
-    root.style.setProperty('--scroll-width', pxProgress )
+	// console.log(pxProgress)
+	root.style.setProperty('--scroll-width', pxProgress)
+
+	if (scroll > 80) {
+		btn.classList.add('active')
+	} else {
+		btn.classList.remove('active')
+	}
+}
+
+const scrollToTop = () => {
+	window.scroll({
+		top: 0,
+	})
 }
 
 addEventListener('scroll', scrollRefresh)
+btn.addEventListener('click', scrollToTop)
