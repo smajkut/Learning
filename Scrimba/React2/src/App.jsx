7c3857448be8349230b6e191ad0d6e0430a3179e
Slawomir <smajkut94@gmail.com>
2023-01-07 16:14:01 +0100

    React project 1

---
+++ Scrimba/React2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function Test() {
+	return (
+		<div>
+			<Navbar />
+			<Main />
+		</div>
+	)
+}
