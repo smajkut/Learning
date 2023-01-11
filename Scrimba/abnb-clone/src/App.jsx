eaa479a5688f697d58fbcb105602f1807af8e14d
Slawomir <smajkut94@gmail.com>
2023-01-11 18:23:09 +0100

    Crearting components in AirBnbClone

---
+++ Scrimba/abnb-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "../src/Navbar"
+import Hero from "../src/Hero"
+import Card from "../src/Card"
+
+export default function App() {
+  return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Card />
+        </div>
+  )
+}
+
