51449512f89327580cd881c2d695af2046fc6182
Slawomir <smajkut94@gmail.com>
2023-01-12 21:12:36 +0100

    Update abnb-clone - added single card

--- Scrimba/abnb-clone/src/App.jsx
+++ Scrimba/abnb-clone/src/App.jsx
@@ -8,8 +8,26 @@ export default function App() {
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <Card 
+              img="../images/katie-zaferes.png"
+              rating="5.0"
+              reviewCount="(6)"
+              country="USA"
+              title="Life Lessons with Katie Zaferes"
+              price="136"
+            />
         </div>
   )
 }
 
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
--- Scrimba/abnb-clone/src/Card.jsx
+++ Scrimba/abnb-clone/src/Card.jsx
@@ -1,26 +1,32 @@
-import React from "react"
+import React from 'react'
+
+
+export default function Card(props) {
+	return (
+		<div className='card'>
+			<img src={props.img} className='card--image' />
+			<div className='card--stats'>
+				<img src='../images/star.png' className='card--star' />
+				<span>{props.rating}</span>
+				<span className='gray'> {props.reviewCount} • </span>
+				<span className='gray'>{props.country}</span>
+			</div>
+			<p>{props.title}</p>
+			<p>
+				<span className='bold'>From ${props.price}</span> / person
+			</p>
+		</div>
+	)
+}
 
 /*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
+Challenge: Pass props to the Card component and display that data
 
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
 */
-
-export default function Card() {
-    return (
-        <h1>Replace this</h1>
-    )
-}
