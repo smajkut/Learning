e08a475fde9324643959d3e7b3d673d8278678be
Slawomir <smajkut94@gmail.com>
2023-01-13 22:27:23 +0100

    Style the box with cards

--- Scrimba/abnb-clone/src/App.jsx
+++ Scrimba/abnb-clone/src/App.jsx
@@ -5,21 +5,24 @@ import Card from '../src/Card'
 import data from '../data'
 
 export default function App() {
-	const cardData = data.map(card => {
-		return <Card img = {card.coverImg}
-		rating={card.stats.rating}
-		reviewCount={card.stats.reviewCount}
-		location={card.location}
-		title={card.title}
-		// price="136"
-    />
+	const cardData = data.map((card) => {
+		return (
+			<Card
+				img={card.coverImg}
+				rating={card.stats.rating}
+				reviewCount={card.stats.reviewCount}
+				location={card.location}
+				title={card.title}
+				price={card.price}
+			/>
+		)
 	})
 
 	return (
 		<div>
 			<Navbar />
 			<Hero />
-			<div>{cardData}</div>
+			<section className='cards-list'>{cardData}</section>
 		</div>
 	)
 }
--- Scrimba/abnb-clone/src/Card.jsx
+++ Scrimba/abnb-clone/src/Card.jsx
@@ -8,7 +8,7 @@ export default function Card(props) {
 			<div className='card--stats'>
 				<img src='../images/star.png' className='card--star' />
 				<span>{props.rating}</span>
-				<span className='gray'> {props.reviewCount} • </span>
+				<span className='gray'>({props.reviewCount}) • </span>
 				<span className='gray'>{props.location}</span>
 			</div>
 			<p>{props.title}</p>
