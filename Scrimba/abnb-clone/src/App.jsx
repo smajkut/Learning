45d156117a9a01b7f0b06d4ec0d0e78fb51f241e
Slawomir <smajkut94@gmail.com>
2023-01-17 20:52:02 +0100

    Finished abnb-clone project

--- Scrimba/abnb-clone/src/App.jsx
+++ Scrimba/abnb-clone/src/App.jsx
@@ -8,12 +8,8 @@ export default function App() {
 	const cardData = data.map((card) => {
 		return (
 			<Card
-				img={card.coverImg}
-				rating={card.stats.rating}
-				reviewCount={card.stats.reviewCount}
-				location={card.location}
-				title={card.title}
-				price={card.price}
+				key={card.id}
+				{...card}
 			/>
 		)
 	})
